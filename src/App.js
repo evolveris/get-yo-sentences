df31fd4f272d3425978ca573713eacd2ca316276
evolveris <corneliastn@gmail.com>
2019-08-13 09:27:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [listOfSentences, setListOfSentences] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [author, setAuthor] = React.useState("");
+
+  const fetchSentences = () => {
+    setIsLoading(true);
+
+    fetch(`http://openlibrary.org/search.json?author=${author}`)
+      .then(data => {
+        return data.json();
+      })
+      .then(authorJsonData => {
+        const listOfSentences = authorJsonData.docs
+          .filter(book => {
+            return book.hasOwnProperty("first_sentence");
+          })
+          .map(book => {
+            return book.first_sentence[0];
+          });
+        setListOfSentences(listOfSentences);
+        setIsLoading(false);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Get👏Yo👏Sentences👏</h3>
+        <div className="App-form">
+          <label>Author:</label>
+          <input
+            onChange={e => {
+              setAuthor(e.target.value);
+            }}
+            placeholder="Who?"
+          />
+          <button onClick={fetchSentences}>Give 'em to me 🛎</button>
+        </div>
       </header>
+      <main className="App-body">
+        <div>
+          <span>
+            {isLoading ? (
+              "Please wait. Fetching sentences."
+            ) : (
+              <ul>
+                {listOfSentences.map(sentence => (
+                  <li>{sentence}</li>
+                ))}
+              </ul>
+            )}
+          </span>
+        </div>
+      </main>
     </div>
   );
 }
